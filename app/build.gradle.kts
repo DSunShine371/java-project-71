5c8cf3dc3212c79e10ceb9b34323669928d6fd69
DSunShine371 <pestic077@gmail.com>
2025-03-25 17:37:25 +0300

    Step 1 Project initialization

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    application
+    java
+    checkstyle
+    jacoco
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("io.freefair.lombok") version "8.13"
+}
+
+group = "hexlet.code"
+version = "1.0"
+
+application {
+    mainClass.set("hexlet.code.Main")
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.1")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+    }
+}
