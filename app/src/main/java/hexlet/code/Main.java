e346b2aa86cee91d82ca8824dee40dccbd9904df
DSunShine371 <pestic077@gmail.com>
2025-03-26 14:22:31 +0300

    Step 4 Arguments and options

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -2,20 +2,39 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "app",
         mixinStandardHelpOptions = true,
         version = "app 1.0",
-        description = "Printing message 'Hello World!'."
+        description = "Compares two configuration files and shows a difference.",
+        customSynopsis = "app [-hV] [-f=format] filepath1 filepath2"
 )
 public class Main implements Runnable {
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format"
+    )
+    private String format = "stylish";
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+
     public static void main(String[] args) {
         new CommandLine(new Main()).execute(args);
     }
 
     @Override
     public void run() {
-        System.out.println("Hello World!");
+        System.out.println("Running with format: " + format);
+        System.out.println("Filepath1: " + filepath1);
+        System.out.println("Filepath2: " + filepath2);
     }
 }
